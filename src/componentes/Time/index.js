1229fe37a486b605a55a1a93b35b655affb53538
Gleison Dias <Gleison_2330@outlook.com>
2026-01-19 19:46:33 -0300

    feat: implementa lógica de exclusão no botão

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -7,8 +7,7 @@ const Time = (props) => {
             <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
             <div className='colaboradores'>
                 {props.colaboradores.map( colaborador => {
-                    console.log('renderizando colaborador');
-                    return <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>} )}
+                    return <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} aoDeletar={props.aoDeletar} />} )}
             </div>
         </section>
     )
